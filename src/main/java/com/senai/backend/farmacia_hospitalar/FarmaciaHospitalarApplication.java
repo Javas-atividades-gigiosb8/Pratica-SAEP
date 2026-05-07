09d9a41ef985715ec0a4e101d106b61f7566c714
giosb8 <giovanabaltar08@gmail.com>
2026-05-07 11:51:57 -0300

    tentando acertar

---
+++ src/main/java/com/senai/backend/farmacia_hospitalar/FarmaciaHospitalarApplication.java
@@ -0,0 +1,13 @@
+package com.senai.backend.farmacia_hospitalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaHospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaHospitalarApplication.class, args);
+	}
+
+}
